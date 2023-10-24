0642241422edecb5803b18bd093249dc597a9652
Ivan Goncharuk <ivangoncharuk.biz@gmail.com>
2023-10-24 17:38:15 -0400

    Implemented keywords, kind, value, and position

--- src/lexer.py
+++ src/lexer.py
@@ -23,14 +23,15 @@ class Token:
         value_str = f": {self.value}" if self.value is not None else ""
         return f"{position_str} {self.kind}{value_str}"
 
-
 class Lexer:
+    keywords = {"if", "then", "else", "end", "while", "do", "print"}
     def __init__(self, input_text):
         self.input_text = input_text
         self.current_char = self.input_text[0] if self.input_text else None
         self.line_number = 1
         self.char_number = 1
         self.index = 0
+        self.current_token = None
 
     def next_char(self):
         if self.current_char == "\n":
@@ -51,12 +52,22 @@ class Lexer:
             return self.input_text[peek_index]
         return None
 
+    def kind(self):
+        return self.current_token.kind if self.current_token else None
+
+    def value(self):
+        return self.current_token.value if self.current_token else None
+
+    def position(self):
+        return self.current_token.position if self.current_token else None
+
     def next_token(self):
         while self.current_char and self.current_char.isspace():
             self.next_char()
 
         if not self.current_char:
-            return Token("end-of-text", position=(self.line_number, self.char_number))
+            self.current_token = Token("end-of-text", position=(self.line_number, self.char_number))
+            return
 
         token_position = (self.line_number, self.char_number)
 
@@ -65,22 +76,24 @@ class Lexer:
             while self.current_char and (self.current_char.isalnum() or self.current_char == "_"):
                 self.next_char()
             identifier = self.input_text[start_index:self.index]
-            return Token("ID", value=identifier, position=token_position)
+            if identifier in self.keywords:
+                self.current_token = Token(identifier, position=token_position)
+            else:
+                self.current_token = Token("ID", value=identifier, position=token_position)
 
         elif self.current_char.isdigit():
             start_index = self.index
             while self.current_char and self.current_char.isdigit():
                 self.next_char()
             number = self.input_text[start_index:self.index]
-            return Token("NUM", value=int(number), position=token_position)
+            self.current_token = Token("NUM", value=int(number), position=token_position)
+
+        elif self.current_char == ":" and self.peek_char() == "=":
+            self.next_char()
+            self.next_char()
+            self.current_token = Token(":=", position=token_position)
 
         else:
-            if self.current_char == ":" and self.peek_char() == "=":
-                token_position = (self.line_number, self.char_number)
-                self.next_char()
-                self.next_char()
-                return Token(":=", position=token_position)
-            
             char = self.current_char
             self.next_char()
-            return Token(char, position=token_position)
+            self.current_token = Token(char, position=token_position)
--- src/test_lexer.py
+++ src/test_lexer.py
@@ -1,8 +1,13 @@
 from lexer import Lexer, Token
 
 test_input_1 = "var1 := 123; x := 456; y := 789"
-lexer = Lexer(test_input_1)
-token = lexer.next_token()
-while token and token.kind != "end-of-text":
-    print(token)
-    token = lexer.next_token()
+test_input_2 = "if x < 10 then y := x + 1 end"
+test_input_3 = "// This is a comment\nx := 5 // Another comment"
+
+
+lexer = Lexer(test_input_2)
+lexer.next_token()
+print(lexer.position(), lexer.kind(), lexer.value() if lexer.value() is not None else "")
+while lexer.kind() != 'end-of-text':
+    lexer.next_token()
+    print(lexer.position(), lexer.kind(), lexer.value() if lexer.value() is not None else "")
