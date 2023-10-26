fa0d72cf4bb11de35b1342019bea24077466d85c
Ivan Goncharuk <ivangoncharuk.biz@gmail.com>
2023-10-26 16:11:19 -0400

    String literals work with ' and "

--- src/lexer.py
+++ src/lexer.py
@@ -23,7 +23,7 @@ class Lexer:
         self.additive_operators = {'+', '-', 'or'}
         self.unary_operators = {'-', 'not'}
         self.assignment_operators = {':='}
-        self.other_symbols = {',',':', ';', '.', '(', ')'}
+        self.other_symbols = {',',':', ';', '.', '(', ')', '\'', '\"'}
         self.keywords = {'program', 'if', 'then', 'end', 'do', 'while',
                          'print', 'else', 'int', 'bool', "and", "or", "not"}
 
@@ -123,13 +123,14 @@ class Lexer:
     def read_string_literal(self):
         """
         Read a string literal from the input text.
-        String literals are enclosed in double quotes.
+        String literals are enclosed in single or double quotes.
         """
         start_line, start_char = self.line_number, self.char_number
         start_index = self.index  # start at the opening quotation mark
+        quote_type = self.current_char  # Save the type of quote used to start the string
         self.next_char()
         
-        while self.current_char and self.current_char != '"':
+        while self.current_char and self.current_char != quote_type:
             if self.current_char in ['\n', '\t']:
                 self.char_number = 1 if self.current_char == '\n' else self.char_number + 4
                 self.line_number += 1 if self.current_char == '\n' else self.line_number
@@ -138,10 +139,13 @@ class Lexer:
             self.index += 1
             self.current_char = self.input_text[self.index] if self.index < len(self.input_text) else None
         
+        if not self.current_char:
+            raise SyntaxError(f"Unclosed string literal at position ({start_line}, {start_char})")
+        
         self.next_char()  # skip the closing quotation mark
-        value = self.input_text[start_index:self.index]
+        value = self.input_text[start_index+1:self.index-1]  # Exclude the quotes from the value
         return Token("STRING", value=value, position=(start_line, start_char))
-    
+
     def skip_whitespace_and_comments(self):
         """
         Skip whitespaces and single-line comments in the input text.
@@ -175,7 +179,7 @@ class Lexer:
         """
         Generate the next token based on the current character in the input text.
         """
-        if self.current_char == '"':
+        if self.current_char in {'"', "'"}:
             self.current_token = self.read_string_literal()
         elif self.current_char.isalpha() or self.current_char == "_":
             self.current_token = self.read_identifier_or_keyword()
